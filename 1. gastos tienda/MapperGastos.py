b8bf6ab192592465435380a57c3552efcaf29739
elmaxterandrius <elmaxterandrius@gmail.com>
2025-01-08 22:40:53 -0500

    correccion

--- 1. gastos tienda/MapperGastos.py
+++ 1. gastos tienda/MapperGastos.py
@@ -6,7 +6,6 @@ for linea in sys.stdin:
     # Elimina espacios y separa los campos
     linea = linea.strip()
     persona, tienda, gasto = linea.split(";")
-
     # Emite la clave (persona;tienda) y el gasto
     print(f"{persona};{tienda}\t{gasto}")
 
